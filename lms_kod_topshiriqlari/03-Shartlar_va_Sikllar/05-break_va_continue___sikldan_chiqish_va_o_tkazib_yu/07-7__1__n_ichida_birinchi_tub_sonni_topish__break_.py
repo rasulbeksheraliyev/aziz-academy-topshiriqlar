dfa1b24b6a1d1fd4a62ec77facec4ba2799dd89f
Rasulbek Sheraliyev <dgatm44@gmail.com>
2026-09-21 13:44:53 +0300

    Add: 7) 1..n ichida birinchi tub sonni topish (break) [rasulbeksheraliyev]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/07-7__1__n_ichida_birinchi_tub_sonni_topish__break_.py
@@ -0,0 +1,14 @@
+n = int(input())
+found = False 
+for i in range(2, n + 1):
+    is_prime = True
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i)
+        found = True
+        break
+if not found:
+    print("No")
